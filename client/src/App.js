c230a39e4ffdafa1c3e2d0fee54b3cacc880bd84
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-11-16 16:27:44 -0700

    added nomatch page to routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
-import React, {Fragment} from 'react'
-import { Route, Link } from 'react-router-dom'
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
+import NoMatch from './components/NoMatch'
 
 
 const App = () => (
-  <>
-  <Link exact to="/">Home</Link>
-  <br></br>
-  <Link to="/about">About</Link>
-
-  <Route exact path="/" component={Home} />
-  <Route exact path="/about" component={About} />
-  </>
+  <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route component={NoMatch} />
+  </Switch>
 )
 
 export default App
