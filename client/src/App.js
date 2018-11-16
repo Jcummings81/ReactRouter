add7a5934f7b174044ee88708ae2c4462f9c5cf7
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-11-16 16:18:49 -0700

    added links to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
+import React, {Fragment} from 'react'
+import { Route, Link } from 'react-router-dom'
 import Home from './components/Home'
+import About from './components/About'
+
 
 const App = () => (
+  <>
+  <Link exact to="/">Home</Link>
+  <br></br>
+  <Link to="/about">About</Link>
+
   <Route exact path="/" component={Home} />
+  <Route exact path="/about" component={About} />
+  </>
 )
 
 export default App
