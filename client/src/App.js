0762f9af860a674448b62fde8782a3a1a0835c87
Jesse Cummings <jesse.cummings.1981@gmail.com>
2018-11-17 11:14:28 -0700

    added ProtectedRoute to protect data if not logged in

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import NavBar from './components/NavBar'
 import Login from './components/Login'
 import Dashboard from './components/Dashboard'
 import Product from './components/Product'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => (
   <>
@@ -14,9 +15,9 @@ const App = () => (
   <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
-      <Route exact path="/dashboard" component={Dashboard} />
       <Route exact path="/about" component={About} />
-      <Route exact path="/products/:id" component={Product} />
+      <ProtectedRoute exact path="/dashboard" component={Dashboard} />
+      <ProtectedRoute exact path="/products/:id" component={Product} />
       <Route component={NoMatch} />
   </Switch>
   </>
